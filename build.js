abaa7d6fdc1db721b369b56bf5d6c9be1205549b
Sam Siggs <samsiggs00@gmail.com>
2026-05-23 14:47:42 +1200

    feat(structure): Better structure

--- build.js
+++ build.js
@@ -1,29 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-const html = fs.readFileSync('index.html', 'utf8');
+const html = fs.readFileSync('src/index.html', 'utf8');
 
 const before = html.split('<div id="sections-root"></div>')[0];
-const after = html.split('sections/loader.js')[1];
+const after = html.split('<script src="sections/loader.js" defer></script>\n')[1];
 
 let combined = before + '<div id="sections-root">\n';
 
 for (let i = 1; i <= 30; i++) {
-  const sectionHtml = fs.readFileSync(path.join('sections', i + '.html'), 'utf8');
+  const sectionHtml = fs.readFileSync(path.join('src/sections', i + '.html'), 'utf8');
   combined += '\n' + sectionHtml;
 }
 
 combined += '\n</div>\n' + after;
 
-fs.copyFile('styles.css', 'dist/styles.css', (err) => {
-  if (err) throw err;
-  console.log('styles.css copied successfully!');
-});
-
-fs.copyFile('script.js', 'dist/script.js', (err) => {
-  if (err) throw err;
-  console.log('script.js copied successfully!');
-});
-
 fs.writeFileSync('dist/index.html', combined, 'utf8');
-console.log('Written index.static.html (' + combined.split('\n').length + ' lines)');
+fs.copyFileSync('src/styles.css', 'dist/styles.css');
+fs.copyFileSync('src/script.js', 'dist/script.js');
+console.log('Built dist/index.html (' + combined.split('\n').length + ' lines)');
